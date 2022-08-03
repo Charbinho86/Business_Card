6353121cffcbe63ac036fa756c6aa73fa682c2c6
Charbio Silva <95384632+Charbinho86@users.noreply.github.com>
2022-08-03 18:03:32 -0300

    Criação das bibliotecas, classes de persistência do Room, ícone do App e a parte funcional para inclusão, compartilhamento dos cartões.

--- app/src/main/java/com/example/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/ui/MainActivity.kt
@@ -1,16 +1,67 @@
 package com.example.businesscard.ui
 
+import android.Manifest
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.businesscard.R
+import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
+import com.example.businesscard.App
+import com.example.businesscard.data.BusinessCardDao
 import com.example.businesscard.databinding.ActivityMainBinding
+import com.example.businesscard.util.Image
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
+    private val adapter by lazy { BusinessCardAdapter() }
+    private lateinit var analytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        analytics = Firebase.analytics
+        setUpPermissions()
+        binding.rvCardView.adapter = adapter
+        getAllBusinessCard()
+        insertListener()
+    }
+
+    private fun setUpPermissions() {
+        // Write permission to access the storage
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            1
+        )
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            1
+        )
+    }
+
+    private fun insertListener() {
+        binding.fabAdd.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddBusinessCard::class.java)
+            startActivity(intent)
+        }
+        adapter.listenerShare = { card ->
+            Image.share(this@MainActivity, card)
+        }
+    }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this) { businessCards ->
+            adapter.submitList(businessCards)
+        }
     }
 }
